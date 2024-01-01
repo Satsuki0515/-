951e3b5bfaf7ff3c41fe7274ee8f48ca6ff4c300
lionn1207 <lion1207ftb@gmail.com>
2024-01-01 11:39:59 +0900

    hissukinou5notuika

--- MoveChara.java
+++ MoveChara.java
@@ -1,143 +1,162 @@
+import javafx.animation.AnimationTimer;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
-import javafx.animation.AnimationTimer;
 
 public class MoveChara {
-    public static final int TYPE_DOWN = 0;
-    public static final int TYPE_LEFT = 1;
-    public static final int TYPE_RIGHT = 2;
-    public static final int TYPE_UP = 3;
 
-    private final String[] directions = { "Down", "Left", "Right", "Up" };
-    private final String[] animationNumbers = { "1", "2", "3" };
-    private final String pngPathPre = "png/cat";
-    private final String pngPathSuf = ".png";
+  public static final int TYPE_DOWN = 0;
+  public static final int TYPE_LEFT = 1;
+  public static final int TYPE_RIGHT = 2;
+  public static final int TYPE_UP = 3;
 
-    private int posX;
-    private int posY;
+  private final String[] directions = { "Down", "Left", "Right", "Up" };
+  private final String[] animationNumbers = { "1", "2", "3" };
+  private final String pngPathPre = "png/cat";
+  private final String pngPathSuf = ".png";
 
-    private MapData mapData;
+  private int posX;
+  private int posY;
 
-    private Image[][] charaImages;
-    private ImageView[] charaImageViews;
-    private ImageAnimation[] charaImageAnimations;
+  private MapData mapData;
 
-    private int charaDirection;
+  private Image[][] charaImages;
+  private ImageView[] charaImageViews;
+  private ImageAnimation[] charaImageAnimations;
 
-    MoveChara(int startX, int startY, MapData mapData) {
-        this.mapData = mapData;
+  private int charaDirection;
 
-        charaImages = new Image[4][3];
-        charaImageViews = new ImageView[4];
-        charaImageAnimations = new ImageAnimation[4];
+  MoveChara(int startX, int startY, MapData mapData) {
+    this.mapData = mapData;
 
-        for (int i = 0; i < 4; i++) {
-            charaImages[i] = new Image[3];
-            for (int j = 0; j < 3; j++) {
-                charaImages[i][j] = new Image(
-                        pngPathPre + directions[i] + animationNumbers[j] + pngPathSuf);
-            }
-            charaImageViews[i] = new ImageView(charaImages[i][0]);
-            charaImageAnimations[i] = new ImageAnimation(
-                    charaImageViews[i], charaImages[i]);
-        }
+    charaImages = new Image[4][3];
+    charaImageViews = new ImageView[4];
+    charaImageAnimations = new ImageAnimation[4];
 
-        posX = startX;
-        posY = startY;
-
-        setCharaDirection(TYPE_RIGHT); // start with right-direction
+    for (int i = 0; i < 4; i++) {
+      charaImages[i] = new Image[3];
+      for (int j = 0; j < 3; j++) {
+        charaImages[i][j] =
+          new Image(
+            pngPathPre + directions[i] + animationNumbers[j] + pngPathSuf
+          );
+      }
+      charaImageViews[i] = new ImageView(charaImages[i][0]);
+      charaImageAnimations[i] =
+        new ImageAnimation(charaImageViews[i], charaImages[i]);
     }
 
-    // set the cat's direction
-    public void setCharaDirection(int cd) {
-        charaDirection = cd;
-        for (int i = 0; i < 4; i++) {
-            if (i == charaDirection) {
-                charaImageAnimations[i].start();
-            } else {
-                charaImageAnimations[i].stop();
-            }
-        }
+    posX = startX;
+    posY = startY;
+
+    setCharaDirection(TYPE_RIGHT); // start with right-direction
+  }
+
+  // set the cat's direction
+  public void setCharaDirection(int cd) {
+    charaDirection = cd;
+    for (int i = 0; i < 4; i++) {
+      if (i == charaDirection) {
+        charaImageAnimations[i].start();
+      } else {
+        charaImageAnimations[i].stop();
+      }
+    }
+  }
+
+  // check whether the cat can move on
+  private boolean isMovable(int dx, int dy) {
+    int nextX = posX + dx;
+    int nextY = posY + dy;
+
+    if (mapData.getMap(nextX, nextY) == MapData.TYPE_WALL) {
+      return false;
+    } else if (
+      mapData.getMap(nextX, nextY) == MapData.TYPE_SPACE ||
+      mapData.getMap(nextX, nextY) == MapData.TYPE_GOAL
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  // move the cat
+  //changed code
+  // move the cat
+  public boolean move(int dx, int dy) {
+    if (isMovable(dx, dy)) {
+      posX += dx;
+      posY += dy;
+      System.out.println("chara[X,Y]:" + posX + "," + posY);
+
+      // Check if the character reached the goal
+      if (mapData.getMap(posX, posY) == MapData.TYPE_GOAL) {
+        System.out.println("Game Clear!!");
+        StageDB.getMainStage().hide();
+        StageDB.getMainSound().stop();
+      }
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // getter: direction of the cat
+  public ImageView getCharaImageView() {
+    return charaImageViews[charaDirection];
+  }
+
+  // getter: x-positon of the cat
+  public int getPosX() {
+    return posX;
+  }
+
+  // getter: y-positon of the cat
+  public int getPosY() {
+    return posY;
+  }
+
+  // Show the cat animation
+  private class ImageAnimation extends AnimationTimer {
+
+    private ImageView charaView = null;
+    private Image[] charaImages = null;
+    private int index = 0;
+
+    private long duration = 500 * 1000000L; // 500[ms]
+    private long startTime = 0;
+
+    private long count = 0L;
+    private long preCount;
+    private boolean isPlus = true;
+
+    public ImageAnimation(ImageView charaView, Image[] images) {
+      this.charaView = charaView;
+      this.charaImages = images;
+      this.index = 0;
     }
 
-    // check whether the cat can move on
-    private boolean isMovable(int dx, int dy) {
-        if (mapData.getMap(posX + dx, posY + dy) == MapData.TYPE_WALL) {
-            return false;
-        } else if (mapData.getMap(posX + dx, posY + dy) == MapData.TYPE_SPACE) {
-            return true;
-        }
-        return false;
-    }
+    @Override
+    public void handle(long now) {
+      if (startTime == 0) {
+        startTime = now;
+      }
 
-    // move the cat
-    public boolean move(int dx, int dy) {
-        if (isMovable(dx, dy)) {
-            posX += dx;
-            posY += dy;
-	    System.out.println("chara[X,Y]:" + posX + "," + posY);
-            return true;
+      preCount = count;
+      count = (now - startTime) / duration;
+      if (preCount != count) {
+        if (isPlus) {
+          index++;
         } else {
-            return false;
+          index--;
         }
-    }
-
-    // getter: direction of the cat
-    public ImageView getCharaImageView() {
-        return charaImageViews[charaDirection];
-    }
-
-    // getter: x-positon of the cat
-    public int getPosX() {
-        return posX;
-    }
-
-    // getter: y-positon of the cat
-    public int getPosY() {
-        return posY;
-    }
-
-    // Show the cat animation
-    private class ImageAnimation extends AnimationTimer {
-
-        private ImageView charaView = null;
-        private Image[] charaImages = null;
-        private int index = 0;
-
-        private long duration = 500 * 1000000L; // 500[ms]
-        private long startTime = 0;
-
-        private long count = 0L;
-        private long preCount;
-        private boolean isPlus = true;
-
-        public ImageAnimation(ImageView charaView, Image[] images) {
-            this.charaView = charaView;
-            this.charaImages = images;
-            this.index = 0;
+        if (index < 0 || 2 < index) {
+          index = 1;
+          isPlus = !isPlus; // true == !false, false == !true
         }
-
-        @Override
-        public void handle(long now) {
-            if (startTime == 0) {
-                startTime = now;
-            }
-
-            preCount = count;
-            count = (now - startTime) / duration;
-            if (preCount != count) {
-                if (isPlus) {
-                    index++;
-                } else {
-                    index--;
-                }
-                if (index < 0 || 2 < index) {
-                    index = 1;
-                    isPlus = !isPlus; // true == !false, false == !true
-                }
-                charaView.setImage(charaImages[index]);
-            }
-        }
+        charaView.setImage(charaImages[index]);
+      }
     }
+  }
 }
